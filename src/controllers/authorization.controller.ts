3682ad89247785218b25009b40798f1277884577
Julius Kiano <jgkiano@gmail.com>
2021-09-26 17:16:13 +0300

    use ApiKeyCreationGuard

--- src/controllers/authorization.controller.ts
+++ src/controllers/authorization.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { GenerateApiKeyDto } from '../models/GenerateApiKeyDto';
 import { v4 as uuidv4 } from 'uuid';
+import { ApiKeyCreationGuard } from '../guards/apiKeyCreationGuard';
 
 @Controller()
+@UseGuards(ApiKeyCreationGuard)
 export class AuthorizationController {
   @Post('/generate-api-key')
   generateApiKey(@Body() credentials: GenerateApiKeyDto) {
