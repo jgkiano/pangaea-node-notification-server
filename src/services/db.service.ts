4560dde6e640fa3c15632feaf2b2e5af425205bc
Julius Kiano <jgkiano@gmail.com>
2021-09-26 11:42:49 +0300

    added logs for redis connection states

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -3,18 +3,30 @@ import validator from 'validator';
 import { createClient } from 'redis';
 import { RedisClientType } from 'redis/dist/lib/client';
 
+/**
+ * redis states:
+ * connect: re-connecting
+ * ready: re-connection was successful
+ * reconnecting: re-connecting
+ * error: connection lost
+ * end: ??
+ */
+
 @Injectable()
 export class DBService {
   private static redisClient: RedisClientType;
   private static REDIS_HOST = process.env.REDIS_HOST || 'localhost';
   private static REDIS_PORT = process.env.REDIS_PORT || 6379;
-
   static async initalize(): Promise<void> {
     return new Promise((resolve, reject) => {
       DBService.redisClient = createClient({
         url: `redis://${DBService.REDIS_HOST}:${DBService.REDIS_PORT}`,
       });
-      DBService.redisClient.on('error', reject);
+      DBService.redisClient.on('error', console.log);
+      DBService.redisClient.on('connect', () =>
+        console.log('[redis]: connect...'),
+      );
+      DBService.redisClient.on('ready', () => console.log('[redis]: ready...'));
       DBService.redisClient.connect().then(resolve).catch(reject);
     });
   }
