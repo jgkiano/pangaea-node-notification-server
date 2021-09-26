28a33e233437ec85ead5cfa4730f9f2ea586240c
Julius Kiano <jgkiano@gmail.com>
2021-09-26 13:35:23 +0300

    added private methods

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import validator from 'validator';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { createClient } from 'redis';
+import { v4 as uuidv4 } from 'uuid';
 import { RedisClientType } from 'redis/dist/lib/client';
-
+import { handleException } from '../util';
 /**
  * redis states:
  * connect: re-connecting
@@ -33,10 +33,53 @@ export class DBService {
     });
   }
 
-  isApiKeyValid(apiKey: string): boolean {
-    if (!validator.isUUID(apiKey)) {
-      return false;
+  async isValidApiKey(apiKey: string): Promise<boolean> {
+    const existingApiKey = await DBService.redisClient.get(`apiKey-${apiKey}`);
+    return typeof existingApiKey === 'string';
+  }
+
+  async createApiKey(username: string) {
+    try {
+      if (await this.isExistingUser(username)) {
+        throw new ConflictException('user already exists');
+      }
+      await this.createUser(username);
+      return await this.createUUIDApiKey(username);
+    } catch (error) {
+      handleException(error);
     }
-    return true;
+  }
+
+  private async createUser(username: string) {
+    try {
+      await DBService.redisClient.set(
+        `user-${username}`,
+        JSON.stringify({ username, createdAt: Date.now(), id: uuidv4() }),
+      );
+    } catch (error) {
+      handleException(error);
+    }
+  }
+
+  private async createUUIDApiKey(username: string): Promise<string> {
+    try {
+      const apiKey = uuidv4();
+      await DBService.redisClient.set(
+        `apiKey-${username}`,
+        JSON.stringify({ userId: username, createdAt: Date.now(), apiKey }),
+      );
+      return apiKey;
+    } catch (error) {
+      handleException(error);
+    }
+  }
+
+  private async isExistingUser(username: string) {
+    try {
+      const existingUser = await DBService.redisClient.get(`user-${username}`);
+      return typeof existingUser === 'string';
+    } catch (error) {
+      return null; // TODO: check if redis connection issue
+    }
   }
 }
