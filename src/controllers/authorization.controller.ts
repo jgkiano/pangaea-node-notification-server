a88b3a33e576c12f2c83cfce0c7f4f22bd410705
Julius Kiano <jgkiano@gmail.com>
2021-09-26 17:24:17 +0300

    generate key from db service

--- src/controllers/authorization.controller.ts
+++ src/controllers/authorization.controller.ts
@@ -2,12 +2,17 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { GenerateApiKeyDto } from '../models/GenerateApiKeyDto';
 import { v4 as uuidv4 } from 'uuid';
 import { ApiKeyCreationGuard } from '../guards/apiKeyCreationGuard';
+import { DBService } from '../services/db.service';
 
 @Controller()
 @UseGuards(ApiKeyCreationGuard)
 export class AuthorizationController {
+  constructor(private readonly dbService: DBService) {}
+
   @Post('/generate-api-key')
-  generateApiKey(@Body() credentials: GenerateApiKeyDto) {
-    return { apiKey: uuidv4() };
+  async generateApiKey(@Body() credentials: GenerateApiKeyDto) {
+    return {
+      apiKey: await this.dbService.createApiKey(credentials.username),
+    };
   }
 }
