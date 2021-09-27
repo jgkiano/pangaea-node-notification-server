1f5ca7809a87e4b698db8f283fc13afdae3ffe1d
Julius Kiano <jgkiano@gmail.com>
2021-09-27 13:58:18 +0300

    use PublishTopicDto

--- src/controllers/publish.controller.ts
+++ src/controllers/publish.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from '../guards/authorization.guard';
-import { TopicDto } from '../models/TopicDto';
+import { PublishGuard } from '../guards/publish.guard';
+import {
+  PublishTopicParamDto,
+  PublishTopicBodyDto,
+} from '../models/PublishTopicDto';
 
 @Controller('publish')
 @UseGuards(AuthorizationGuard)
+@UseGuards(PublishGuard)
 export class PublishController {
   @Post(':topic')
-  publish(@Param() params: TopicDto): { message: string } {
-    return { message: params.topic };
+  publish(
+    @Param() params: PublishTopicParamDto,
+    @Body() body: PublishTopicBodyDto,
+  ) {
+    return { topc: params.topic, body };
   }
 }
