53cbb14f36fb80683d8cd3ea58f142e5a9d86dfc
Julius Kiano <jgkiano@gmail.com>
2021-09-27 14:41:23 +0300

    implement subscription topic via db

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthorizationGuard } from '../guards/authorization.guard';
+import {
+  SubscriptionBodyDto,
+  SubscriptionParamDto,
+} from '../models/SubscriptionDto';
+import { DBService } from '../services/db.service';
 
 @Controller('subscribe')
 @UseGuards(AuthorizationGuard)
 export class SubscriptionController {
-  @Post()
-  subscribe(): { message: string } {
-    return { message: 'Hey there' };
+  constructor(private readonly dbService: DBService) {}
+
+  @Post(':topic')
+  async subscribe(
+    @Param() params: SubscriptionParamDto,
+    @Body() body: SubscriptionBodyDto,
+  ): Promise<{ url: string; topic: string }> {
+    return await this.dbService.subscribeTopic(params.topic, body.url);
   }
 }
