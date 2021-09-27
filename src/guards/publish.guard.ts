5222a060e8404b50022b5885c0b442c40c47df36
Julius Kiano <jgkiano@gmail.com>
2021-09-27 13:58:46 +0300

    created PublishGuard

---
+++ src/guards/publish.guard.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
+import { DBService } from '../services/db.service';
+
+@Injectable()
+export class PublishGuard implements CanActivate {
+  constructor(private readonly dbService: DBService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request: Request = context.switchToHttp().getRequest();
+    if (this.isObject(request.body)) {
+      return true;
+    }
+    throw new BadRequestException('request body must be a json object');
+  }
+
+  private isObject(value: unknown) {
+    return (
+      value != null &&
+      (typeof value === 'object' || typeof value === 'function') &&
+      !Array.isArray(value)
+    );
+  }
+}
