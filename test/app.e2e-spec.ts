7b12a4a5ab75602e31f1f6b190bb80421aed09cb
Julius Kiano <jgkiano@gmail.com>
2021-09-28 20:06:15 +0300

    added e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,94 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { SubscriberModule } from '../src/external_subscriber/subscriber.module';
+import { DBService } from '../src/services/db.service';
+import { isUUID } from 'class-validator';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let subscriberApp: INestApplication;
+  let apiKey = '';
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const appModuleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    const subscriberModuleFixture: TestingModule =
+      await Test.createTestingModule({
+        imports: [SubscriberModule],
+      }).compile();
+
+    app = appModuleFixture.createNestApplication();
+    subscriberApp = subscriberModuleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    subscriberApp.useGlobalPipes(new ValidationPipe());
+    await DBService.initalize();
+    await DBService.flush();
     await app.init();
+    await subscriberApp.init();
+  });
+
+  it('app is defined', () => {
+    expect(app).toBeDefined();
+  });
+
+  it('subscriberApp is defined', () => {
+    expect(subscriberApp).toBeDefined();
   });
 
   it('/ (GET)', () => {
+    return request(app.getHttpServer()).get('/').expect(200);
+  });
+
+  it('[Subscriber] / (GET)', () => {
+    return request(subscriberApp.getHttpServer()).get('/').expect(200);
+  });
+
+  it('/generate-api-key (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/generate-api-key')
+      .send({ username: 'kiano' })
+      .expect(201)
+      .then((response) => {
+        apiKey = response.body.apiKey;
+        expect(isUUID(apiKey)).toBe(true);
+      });
+  });
+
+  it('/subscribe/:topic (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/subscribe/food')
+      .set({ authorization: apiKey, Accept: 'application/json' })
+      .send({ url: 'http://localhost:4000/test1' })
+      .expect(201)
+      .then((response) => {
+        const { topic, url } = response.body;
+        expect(topic).toEqual('food');
+        expect(url).toEqual('http://localhost:4000/test1');
+      });
+  });
+
+  it('/publish/:topic (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/publish/food')
+      .set({ authorization: apiKey, Accept: 'application/json' })
+      .send({ message: 'I love food' })
+      .expect(201)
+      .then(async (response) => {
+        expect(response.body.status).toEqual('published');
+      });
+  });
+
+  it('[Subscriber] /:subscriber', async () => {
+    await new Promise((r) => setTimeout(r, 500));
+    request('http://localhost:4000')
+      .get('/test1')
       .expect(200)
-      .expect('Hello World!');
+      .then((response) => {
+        expect(response.body).toBeTruthy(); // TODO: probably use super agant
+      });
   });
 });
