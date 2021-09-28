21cd031927e56912914bddf5c99f0b47ed637619
Julius Kiano <jgkiano@gmail.com>
2021-09-28 18:08:38 +0300

    use nest for external server

---
+++ src/external_subscriber/subscriber.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+import { SuccessPingResponse } from '../types';
+
+@Controller()
+export class SubscriberController {
+  @Get()
+  ping(): SuccessPingResponse {
+    return { message: 'API live! 🕺' };
+  }
+}
