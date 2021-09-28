b6b0ce2f4a7c19f352e4058e42779f9f10f029a5
Julius Kiano <jgkiano@gmail.com>
2021-09-28 11:24:55 +0300

    initial subscriber routes

--- subscriber-server/dist/server.js
+++ subscriber-server/dist/server.js
@@ -1,2 +1,9 @@
 "use strict";
-var response = { message: 'Hey' };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get('/', function (req, res) { return res.send('Hello'); });
+app.listen(5000, function () { return console.log('server running..'); });
--- subscriber-server/src/server.ts
+++ subscriber-server/src/server.ts
@@ -1,1 +1,16 @@
-const response: { message: string } = { message: 'Hey' };
+import express from 'express';
+
+const PORT = process.env.NODE_SUBSCRIBER_PORT || 9000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (_req, res) => res.json({ message: 'subscriber API live! 🥳' }));
+
+app.post('/:subscriber', (req, res) => {
+  const { subscriber } = req.params;
+  res.json({ subscriber, data: req.body });
+});
+
+app.listen(PORT, () =>
+  console.log(`subscriber server running on port: ${PORT}`),
+);
