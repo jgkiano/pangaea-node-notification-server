576407a080f3e2539ba2e54fa4c17e12b1fc86fa
Julius Kiano <jgkiano@gmail.com>
2021-09-28 20:11:46 +0300

    moved constants to the top

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DBService } from './services/db.service';
 import { SubscriberModule } from './external_subscriber/subscriber.module';
+
+const PUBLISHER_SERVER_PORT = process.env.NODE_PORT || 3000;
+const SUBSCRIBER_SERVER_PORT = process.env.SUBSCRIBER_NODE_PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const subscriberApp = await NestFactory.create(SubscriberModule);
   app.useGlobalPipes(new ValidationPipe());
   subscriberApp.useGlobalPipes(new ValidationPipe());
   try {
-    const PUBLISHER_SERVER_PORT = process.env.NODE_PORT || 3000;
-    const SUBSCRIBER_SERVER_PORT = process.env.SUBSCRIBER_NODE_PORT || 4000;
     await DBService.initalize();
     await app.listen(PUBLISHER_SERVER_PORT);
     console.log(`started publisher server on port: ${PUBLISHER_SERVER_PORT}`);
@@ -20,4 +22,5 @@ async function bootstrap() {
     console.log(error);
   }
 }
+
 bootstrap();
